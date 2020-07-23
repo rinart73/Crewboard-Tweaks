12832a23f62153945a61b3ba66ec9f0842b8f9bc
Rinart73 <rinart73@gmail.com>
2020-07-24 00:14:16 +0200

    1.1.2 * Updated to 1.1.2, merged new vanilla UI with mod changes * Removed max Avorion required version to allow to enable mod on any future versions

--- data/scripts/entity/crewboard.lua
+++ data/scripts/entity/crewboard.lua
@@ -1,8 +1,11 @@
+local workforceUI = workforceUI or {} -- bringing 1.1.2 stuff to earlier versions
+
 local Azimuth -- server includes
 local CrewboardTweaksConfig -- client/server
-local crewboardTweaks_isVisible -- client
-local crewboardTweaks_crewWorkforceUI, crewboardTweaks_freeCrewSpaceLabel, crewboardTweaks_hireReqCrewBtn, crewboardTweaks_transportLabel -- client UI
-local crewboardTweaks_initialize, crewboardTweaks_onShowWindow, crewboardTweaks_onCloseWindow, crewboardTweaks_sync -- client extended functions
+local crewboardTweaks_freeCrewSpaceLabel, crewboardTweaks_hireReqCrewBtn, crewboardTweaks_transportLabel -- UI
+local crewboardTweaks_initialize -- client/server extended functions
+local crewboardTweaks_onRequestTransportButtonPressed -- server extended functions
+local crewboardTweaks_onShowWindow, crewboardTweaks_refreshUI -- client extended functions
 
 
 if onClient() then
@@ -11,17 +14,18 @@ if onClient() then
 include("azimuthlib-uiproportionalsplitter")
 
 -- PREDEFINED --
+
 crewboardTweaks_initialize = CrewBoard.initialize
 function CrewBoard.initialize(...)
     crewboardTweaks_initialize(...)
 
-    CrewboardTweaksConfig = {}
+    CrewboardTweaksConfig = { TransportShipMinMissingCrew = 300 } -- default config
     invokeServerFunction("crewboardTweaks_sendSettings")
 end
 
-function CrewBoard.initUI() -- overridden
+function CrewBoard.initUI() -- overriden
     local res = getResolution()
-    local size = vec2(890, 510)
+    local size = vec2(890, 500)
     local menu = ScriptUI()
     local window = menu:createWindow(Rect(res * 0.5 - size * 0.5, res * 0.5 + size * 0.5));
     menu:registerWindow(window, "Hire Crew"%_t);
@@ -30,9 +34,8 @@ function CrewBoard.initUI() -- overridden
     window.showCloseButton = 1
     window.moveable = 1
 
-    local hsplit = UIHorizontalProportionalSplitter(Rect(vec2(0, 10), size), 10, 10, {0.8, 30, 60, 0.2})
-
-    local hmsplit = UIHorizontalMultiSplitter(Rect(hsplit[1].lower - vec2(0, 10), hsplit[1].upper - vec2(0, 10)), 10, 10, 6)
+    local hsplit = UIHorizontalSplitter(Rect(vec2(0, 10), size), 10, 10, 0.8)
+    local lister = UIVerticalLister(hsplit.top, 10, 10)
 
     local padding = 15
     local iconSize = 30
@@ -49,8 +52,8 @@ function CrewBoard.initUI() -- overridden
     local priceX = amountBoxX + amountBoxSize + padding
     local buttonX = priceX + priceSize + padding
 
-    for i = 0, 6 do
-        local rect = hmsplit:partition(i)
+    for i = 0, 5 do
+        local rect = lister:nextRect(30)
 
         local pic = window:createPicture(Rect(iconX, rect.lower.y, iconX + iconSize, rect.upper.y), "")
         local bar = window:createNumbersBar(Rect(barX, rect.lower.y, barX + barSize, rect.upper.y))
@@ -65,59 +68,70 @@ function CrewBoard.initUI() -- overridden
         local button = window:createButton(Rect(buttonX, rect.lower.y, buttonX + buttonSize, rect.upper.y), "Hire"%_t, "onHireButtonPressed")
 
         local hide = function (self)
-            self.bar.visible = false
-            self.pic.visible = false
-            self.slider.visible = false
-            self.box.visible = false
-            self.label.visible = false
-            self.button.visible = false
+            self.bar:hide()
+            self.pic:hide()
+            self.slider:hide()
+            self.box:hide()
+            self.label:hide()
+            self.button:hide()
         end
 
         local show = function (self)
-            self.bar.visible = true
-            self.pic.visible = true
-            self.slider.visible = true
-            self.box.visible = true
-            self.label.visible = true
-            self.button.visible = true
+            self.bar:show()
+            self.pic:show()
+            self.slider:show()
+            self.box:show()
+            self.label:show()
+            self.button:show()
         end
 
-        uiGroups[#uiGroups+1] = {pic=pic, bar=bar, slider=slider, box=box, label=label, button=button, show=show, hide=hide}
+        table.insert(uiGroups, {pic=pic, bar=bar, slider=slider, box=box, label=label, button=button, show=show, hide=hide})
     end
 
-    -- free crew space and 'Hire Required Crew'
-    local vsplit = UIVerticalProportionalSplitter(hsplit[2], 10, 0, {0.5, 150})
-    crewboardTweaks_freeCrewSpaceLabel = window:createLabel(Rect(vsplit[1].lower + vec2(0, 6), vsplit[1].upper), "", 15)
-    crewboardTweaks_hireReqCrewBtn = window:createButton(vsplit[2], "Hire"%_t, "crewboardTweaks_onHireRequiredBtn")
-    crewboardTweaks_hireReqCrewBtn.maxTextSize = 15
+    -- current workforce
+    lister:nextRect(1)
+    local rect = lister:nextRect(30)
+    local vsplit = UIVerticalProportionalSplitter(rect, 10, 0, {0.5, 270})
+    crewboardTweaks_freeCrewSpaceLabel = window:createLabel(vsplit[1], "Current Crew"%_t, 14)
+    crewboardTweaks_freeCrewSpaceLabel:setBottomLeftAligned()
+    rect = vsplit[2]
+    crewboardTweaks_hireReqCrewBtn = window:createButton(Rect(rect.lower, rect.upper + vec2(10, 0)), "Hire required crew"%_t, "crewboardTweaks_onHireRequiredBtn")
+    crewboardTweaks_hireReqCrewBtn.maxTextSize = 16
 
-    -- ship workforce
-    crewboardTweaks_crewWorkforceUI = {}
-    local wfsplit = UIVerticalMultiSplitter(Rect(hsplit[3].lower, hsplit[3].upper - vec2(0, 10)), 10, 0, 5)
-    for i = 1, 12 do
-        local prof = CrewProfession(i)
-        local wfPartition = wfsplit:partition(i - math.floor(i / 7) * 6 - 1)
-        local wfYOffset = math.floor(i / 7) * 30
-        local wfIcon = window:createPicture(Rect(wfPartition.lower + vec2(0, wfYOffset), wfPartition.lower + vec2(20, 20 + wfYOffset)), prof.icon)
-        wfIcon.isIcon = 1
-        wfIcon.tooltip = prof:name()
-        crewboardTweaks_crewWorkforceUI[i] = {
-          icon = wfIcon,
-          label = window:createLabel(Rect(wfPartition.lower + vec2(30, 2 + wfYOffset), wfPartition.upper + vec2(0, wfYOffset)), "0/0", 12)
-        }
+    for row = 0, 1 do
+        local workforceSplit = UIVerticalMultiSplitter(lister:nextRect(25), 10, 0, 6)
+
+        for i = 0, 6 do
+            local professionIndex = i
+            if row == 1 then
+                if i == 6 then break end
+
+                professionIndex = i + 7
+            end
+
+            local profession = CrewProfession(professionIndex)
+            local split = UIVerticalSplitter(workforceSplit:partition(i), 10, 0, 0.5)
+            split:setLeftQuadratic()
+
+            local picture = window:createPicture(split.left, profession.icon)
+            picture.isIcon = true
+            local label = window:createLabel(split.right, "", 14)
+            label:setLeftAligned()
+
+            workforceUI[professionIndex] = {picture = picture, label = label}
+        end
     end
 
-    window:createLine(hsplit[3].bottomLeft, hsplit[3].bottomRight)
+    window:createLine(hsplit.top.bottomLeft, hsplit.top.bottomRight)
 
-    local hsplit2 = UIHorizontalSplitter(hsplit[4], 10, 0, 0.4)
+    -- crew transport
+    local hsplit2 = UIHorizontalSplitter(hsplit.bottom, 10, 0, 0.4)
     local vmsplit = UIVerticalMultiSplitter(hsplit2.bottom, 10, 0, 2)
 
     requestTransportButton = window:createButton(vmsplit:partition(2), "Request Transport"%_t, "onRequestTransportButtonPressed")
 
     local text = "You can request a crew transport ship here containing a complete crew for your current ship.\nOnly possible if your ship needs at least 300 more crew members."%_t
-    if CrewboardTweaksConfig.TransportShipMinMissingCrew then
-        text = text:gsub(300, CrewboardTweaksConfig.TransportShipMinMissingCrew)
-    end
+    text = text:gsub(300, CrewboardTweaksConfig.TransportShipMinMissingCrew)
     crewboardTweaks_transportLabel = window:createLabel(hsplit2.top, text, 12)
     crewboardTweaks_transportLabel.font = FontType.Normal
     crewboardTweaks_transportLabel.wordBreak = true
@@ -135,104 +149,75 @@ function CrewBoard.initUI() -- overridden
     uiInitialized = true
 end
 
+if GameVersion() < Version(1, 1, 2) then
+--
 crewboardTweaks_onShowWindow = CrewBoard.onShowWindow
 function CrewBoard.onShowWindow(...)
     crewboardTweaks_onShowWindow(...)
 
-    crewboardTweaks_isVisible = true
-
-    local ship = getPlayerCraft()
-    ship:registerCallback("onCrewChanged", "crewboardTweaks_onCrewChanged")
+    local craft = Player().craft
+    if craft then
+        craft:registerCallback("onCrewChanged", "onCrewChanged")
+    end
 end
 
 crewboardTweaks_onCloseWindow = CrewBoard.onCloseWindow
 function CrewBoard.onCloseWindow(...)
     if crewboardTweaks_onCloseWindow then crewboardTweaks_onCloseWindow(...) end
 
-    crewboardTweaks_isVisible = false
-end
-
-crewboardTweaks_sync = CrewBoard.sync
-function CrewBoard.sync(available, transport, lineToReset)
-    -- update crew workforce labels
-    if available and crewboardTweaks_isVisible then
-        CrewBoard.crewboardTweaks_updateInfo()
+    local craft = Player().craft
+    if craft then
+        craft:unregisterCallback("onCrewChanged", "onCrewChanged")
     end
-
-    crewboardTweaks_sync(available, transport, lineToReset)
 end
+--
+end
+
+-- FUNCTIONS --
 
 crewboardTweaks_refreshUI = CrewBoard.refreshUI
 function CrewBoard.refreshUI(...)
     crewboardTweaks_refreshUI(...)
 
     if not uiInitialized then return end
-    local player = Player()
-    local ship = player.craft
-    if ship.maxCrewSize == nil or ship.crewSize == nil then return end
-
-    local active = true
-    local tooltip
-    if not CheckFactionInteraction(player.index, 60000) then
-        active = false
-        tooltip = "Your relations with that faction aren't good enough."%_t
-    elseif ship.minCrew.size - ship.crewSize < (CrewboardTweaksConfig.TransportShipMinMissingCrew or 300) then
-        local amount = math.max(0, ship.minCrew.size - ship.crewSize)
-        active = false
-        local text = "We don't require more than 300 additional crew members. Additionally required crew members: ${amount}"%_t
-        if CrewboardTweaksConfig.TransportShipMinMissingCrew then
-            text = text:gsub(300, CrewboardTweaksConfig.TransportShipMinMissingCrew)
-        end
-        tooltip = text % {amount = amount}
-    elseif transportData then
-        active = false
-        tooltip = "There's already a transport on the way."%_t
-    end
-    requestTransportButton.active = active
-    requestTransportButton.tooltip = tooltip
-end
-
--- CALLBACKS --
-function CrewBoard.crewboardTweaks_onCrewChanged(index)
-    if crewboardTweaks_isVisible then
-        local entity = Sector():getEntity(index)
-        if entity then
-            local ship = getPlayerCraft()
-            if entity.index == ship.index then
-                CrewBoard.crewboardTweaks_updateInfo()
-            end
-        end
-    end
-end
-
-function CrewBoard.crewboardTweaks_updateInfo()
-    local ship, freeSpace, buyableCrew, workforce, minWorkforce = CrewBoard.crewboardTweaks_collectData()
+    local ship, buyableCrew = CrewBoard.crewboardTweaks_collectData()
     if not ship then return end
 
-    crewboardTweaks_freeCrewSpaceLabel.caption = "Hire required crew (free space ${num})"%_t % { num = freeSpace }
-    for i = 1, 12 do
-        local prof = CrewProfession(i)
-        local wf = workforce[i] or 0
-        local minWf = minWorkforce[i] or 0
-        local wfUIPair = crewboardTweaks_crewWorkforceUI[i]
-        wfUIPair.label.caption = wf.."/"..minWf
-        if wf < minWf then
-            wfUIPair.label.color = ColorInt(0xffff2626)
-            wfUIPair.label.tooltip = nil
-        else
-            local mult = 1.0
-            if i == CrewProfessionType.Engine or i == CrewProfessionType.Repair then
-                mult = 1.3
-            end
-            if minWf * mult + 2 < wf then -- too much crew
-                wfUIPair.label.color = ColorInt(0xff00b1d1)
-                wfUIPair.label.tooltip = "You have too much crew of this type"%_t
+    crewboardTweaks_freeCrewSpaceLabel.caption = "Current Crew"%_t .. " (free space ${num})"%_t % { num = ship.maxCrewSize - ship.crewSize }
+
+    if GameVersion() < Version(1, 1, 2) then -- bringing 1.1.2 stuff to earlier versions
+        -- current workforce
+        local currentWorkforce = {}
+        for profession, amount in pairs(ship.crew:getWorkforce()) do
+            currentWorkforce[profession.value] = amount
+        end
+
+        local minCrew = ship.minCrew
+        local minWorkforce = {}
+        for profession, amount in pairs(minCrew:getWorkforce()) do
+            minWorkforce[profession.value] = amount
+        end
+
+        for professionIndex, data in pairs(workforceUI) do
+            local required = minWorkforce[professionIndex] or 0
+            local available = currentWorkforce[professionIndex] or 0
+            if required == 0 then
+                data.label.caption = available
             else
-                wfUIPair.label.color = ColorInt(0xffe0e0e0)
-                wfUIPair.label.tooltip = nil
+                data.label.caption = "${workforce}/${required}"%_t % {workforce = available, required = required}
             end
+
+            if required > available then
+                data.label.color = ColorRGB(1, 1, 0)
+            else
+                data.label.color = ColorRGB(1, 1, 1)
+            end
+
+            data.picture.tooltip = CrewProfession(professionIndex):name(available)
         end
     end
+
+    -- hire required crew
     local hireBtnTooltip = {}
     local price = 0
     local stationFaction = Faction()
@@ -251,13 +236,29 @@ function CrewBoard.crewboardTweaks_updateInfo()
         crewboardTweaks_hireReqCrewBtn.tooltip = nil
         crewboardTweaks_hireReqCrewBtn.active = false
     end
-end
 
-function CrewBoard.crewboardTweaks_onHireRequiredBtn()
-    invokeServerFunction("crewboardTweaks_hireRequiredBtn")
+    -- crew transport
+    local active = true
+    local tooltip
+    if not CheckFactionInteraction(Player().index, 60000) then
+        active = false
+        tooltip = "Your relations with that faction aren't good enough."%_t
+    elseif ship.minCrew.size - ship.crewSize < CrewboardTweaksConfig.TransportShipMinMissingCrew then
+        local amount = math.max(0, ship.minCrew.size - ship.crewSize)
+        active = false
+        local text = "We don't require more than 300 additional crew members. Additionally required crew members: ${amount}"%_t
+        text = text:gsub(300, CrewboardTweaksConfig.TransportShipMinMissingCrew)
+        tooltip = text % {amount = amount}
+    elseif transportData then
+        active = false
+        tooltip = "There's already a transport on the way."%_t
+    end
+    requestTransportButton.active = active
+    requestTransportButton.tooltip = tooltip
 end
 
 -- CALLABLE --
+
 function CrewBoard.crewboardTweaks_receiveSettings(serverConfig)
     CrewboardTweaksConfig = serverConfig
 
@@ -269,24 +270,46 @@ function CrewBoard.crewboardTweaks_receiveSettings(serverConfig)
     CrewBoard.refreshUI()
 end
 
+-- CALLBACKS --
+
+if GameVersion() < Version(1, 1, 2) then
+--
+function CrewBoard.onCrewChanged()
+    CrewBoard.refreshUI()
+end
+--
+end
+
+function CrewBoard.crewboardTweaks_onHireRequiredBtn()
+    invokeServerFunction("crewboardTweaks_hireRequiredBtn")
+end
+
 
 else -- onServer
 
 
 Azimuth = include("azimuthlib-basic")
 
-local crewboardTweaks_configOptions = {
-  _version = { default = "1.0", comment = "Config version. Don't touch." },
-  TransportShipMinMissingCrew = { default = 300, min = 0, format = "floor", comment = "Minimal missing crew that is required to call a transport ship." }
-}
-local crewboardTweaks_isModified
-CrewboardTweaksConfig, crewboardTweaks_isModified = Azimuth.loadConfig("CrewboardTweaks", crewboardTweaks_configOptions)
-if crewboardTweaks_isModified then
-    Azimuth.saveConfig("CrewboardTweaks", CrewboardTweaksConfig, crewboardTweaks_configOptions)
+-- PREDEFINED --
+
+crewboardTweaks_initialize = CrewBoard.initialize
+function CrewBoard.initialize(...)
+    crewboardTweaks_initialize(...)
+
+    local crewboardTweaks_configOptions = {
+      _version = { default = "1.0", comment = "Config version. Don't touch." },
+      TransportShipMinMissingCrew = { default = 300, min = 0, format = "floor", comment = "Minimal missing crew that is required to call a transport ship." }
+    }
+    local crewboardTweaks_isModified
+    CrewboardTweaksConfig, crewboardTweaks_isModified = Azimuth.loadConfig("CrewboardTweaks", crewboardTweaks_configOptions)
+    if crewboardTweaks_isModified then
+        Azimuth.saveConfig("CrewboardTweaks", CrewboardTweaksConfig, crewboardTweaks_configOptions)
+    end
 end
 
--- PREDEFINED --
--- Vanilla Fix: It's possible to call a transport ship even if you're missing < 300 people
+-- CALLABLE --
+
+-- Vanilla Fix: It was possible to call a transport ship even if you were missing < 300 people
 crewboardTweaks_onRequestTransportButtonPressed = CrewBoard.onRequestTransportButtonPressed
 function CrewBoard.onRequestTransportButtonPressed(...)
     local buyer, ship = getInteractingFaction(callingPlayer, AlliancePrivilege.SpendResources)
@@ -296,7 +319,6 @@ function CrewBoard.onRequestTransportButtonPressed(...)
     crewboardTweaks_onRequestTransportButtonPressed(...)
 end
 
--- CALLABLE --
 function CrewBoard.crewboardTweaks_sendSettings()
     invokeClientFunction(Player(callingPlayer), "crewboardTweaks_receiveSettings", { TransportShipMinMissingCrew = CrewboardTweaksConfig.TransportShipMinMissingCrew })
 end
@@ -304,7 +326,7 @@ callable(CrewBoard, "crewboardTweaks_sendSettings")
 
 function CrewBoard.crewboardTweaks_hireRequiredBtn()
     local player = Player(callingPlayer)
-    local ship, _, buyableCrew = CrewBoard.crewboardTweaks_collectData(callingPlayer)
+    local ship, buyableCrew = CrewBoard.crewboardTweaks_collectData(callingPlayer)
     if not ship then return end
 
     for k, v in pairs(buyableCrew) do
@@ -321,13 +343,11 @@ function CrewBoard.crewboardTweaks_collectData(playerIndex)
     local player = Player(playerIndex)
     local ship = player and player.craft or nil
     if not ship or not ship:hasComponent(ComponentType.Crew) then return end
-    local freeSpace = ship.maxCrewSize - ship.crewSize
 
     local availableProfs = {}
     for i, pair in pairs(availableCrew) do
         availableProfs[pair.profession] = { number = pair.number, row = i }
     end
-
     local workforce = {}
     for k, v in pairs(ship.crew:getWorkforce()) do
         workforce[k.value] = v
@@ -372,5 +392,5 @@ function CrewBoard.crewboardTweaks_collectData(playerIndex)
         end
     end
 
-    return ship, freeSpace, buyableCrew, workforce, minWorkforce
+    return ship, buyableCrew
 end
